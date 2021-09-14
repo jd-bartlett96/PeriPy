b2ea702f366af9da8eb3649698ea80ef8e5bd801
bb515 <bb515@cam.ac.uk>
2021-09-14 10:55:16 +0100

    changed comment for mesh_file

--- peripy/model.py
+++ peripy/model.py
@@ -62,7 +62,8 @@ class Model(object):
         size of the critical_stretch and bond_stiffness positional arguments.
 
         :arg str mesh_file: Path of the mesh file defining the systems nodes
-            and connectivity.
+            and connectivity, or :class:`numpy.ndarray` or list of lists
+            containing coordinates of nodes.
         :arg  integrator: The integrator to use, see
             :mod:`peripy.integrators` for options.
         :type integrator: :class:`peripy.integrators.Integrator`
